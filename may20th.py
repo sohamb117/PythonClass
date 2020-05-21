4acd93a1cd644754913d5f3fb0edc0a5d1f863bd
Soham Bhattacharya <sohamb117@gmail.com>
2020-05-21 00:16:33 +0000

    May 20th class

---
+++ may20th.py
@@ -0,0 +1,7 @@
+words = input()
+if words == "password":
+	print("You got the password!")
+elif words == "password2":
+	print("You got the second password!")
+else:
+	print("You did not get the password")
