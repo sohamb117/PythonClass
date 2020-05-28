0ffa1f5d6017cd0c907dd7d8884acc754729d1d6
Soham Bhattacharya <sohamb117@gmail.com>
2020-05-28 00:06:41 +0000

    May 27th class

---
+++ may27.py
@@ -0,0 +1,7 @@
+age = 15 # Integer
+age_real = 15.4 # Float
+name = "Soham" # String
+truefalse = True # Boolean
+shopping_list = ["Avocados", "Corn", "Chips"] # List
+
+print("this", "is", "an", "example", sep="-")
